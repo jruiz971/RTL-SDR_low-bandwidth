d065f211f7568f4ed53d7d0f61688d67a9b9e9ef
jruiz971 <juanluisruiz971@gmail.com>
2021-05-27 21:56:22 -0500

    Guardar los valores reales e imaginarios en txt
    Separa los valores de las muestras en reales e imaginarios, los almacena en disco en formato txt

--- read_print_samples.py
+++ read_print_samples.py
@@ -1,13 +1,19 @@
 #Simple way to read and print some samples
 from rtlsdr import RtlSdr
 from math import e
+import numpy as np 
 import sys
 
-def read_print_samples(sample_rate, center_freq, freq_correction):
+#def read_print_samples(sample_rate, center_freq, freq_correction):
+def read_print_samples(center_freq):
+    """
+    input:
+    center_freq.- Get/Set the center frequency of the device (in Hz)
+
+    output:
+    samples.- numpy array
+    """
     sdr = RtlSdr()
-    #input from console
-    
-    #print(type(sample_rate), type(center_freq), type(freq_correction))    
     
     # configure device
     """
@@ -15,13 +21,26 @@ def read_print_samples(sample_rate, center_freq, freq_correction):
     sdr.center_freq = 70e6     # Hz
     sdr.freq_correction = 60   # PPM
     """
-    sdr.sample_rate = sample_rate  # Hz
-    sdr.center_freq = center_freq     # Hz
-    sdr.freq_correction = freq_correction   # PPM
+    sdr.center_freq = center_freq     # Hz 
+
+    sdr.sample_rate =  230000 #66KHz
+    """
+    #Teorema de Nyquist  # Hz
+    sample_rate óptimo: 2B
+    Frecuencia de muestreo debe ser dos veces el ancho de banda
+    """
+    sdr.freq_correction = 1   # PPM
     sdr.gain = 'auto'
     
-    return(sdr.read_samples(512))
+    samples = sdr.read_samples(512) #number  of  samples  or  bytes  to  read 
+
+    np.savetxt('samples_real.txt', samples.real, fmt='%10.15f' )
+    np.savetxt('samples_imag.txt', samples.imag, fmt='%10.15f' )
+    ##Por ahora lo voy a guardar como txt, pero seguiré en la busqueda de un formato mejor para la transferencia de archivos. 
+    ##npy parecía buena opción
+    #np.save('samples.npy',samples)
 
 
 if __name__=="__main__":
-    print(read_print_samples( int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]) ) )
+    #print(read_print_samples( int(sys.argv[1]),int(sys.argv[2]),int(sys.argv[3]) ) )
+    read_print_samples( int(sys.argv[1]) ) #Frecuencia central (estación de radio)
